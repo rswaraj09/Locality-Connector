8348b89f42b56be9d29a866783b32982397c4dfd
Ritik Swaraj <swarajritik@gmail.com>
2026-07-08 22:03:01 +0530

    Add data transfer object (UpdateUserRequest.java)

---
+++ src/main/java/com/example/localityconnector/dto/UpdateUserRequest.java
@@ -0,0 +1,15 @@
+package com.example.localityconnector.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateUserRequest {
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    private String address;
+    private String phoneNumber;
+}
