9587fddfa50901f062c6cd405c10d3aef3d768fb
Ritik Swaraj <swarajritik@gmail.com>
2026-07-08 22:03:04 +0530

    Add data transfer object (UserSignupRequest.java)

---
+++ src/main/java/com/example/localityconnector/dto/UserSignupRequest.java
@@ -0,0 +1,31 @@
+package com.example.localityconnector.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSignupRequest {
+    
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+    
+    @NotBlank(message = "Address is required")
+    private String address;
+    
+    private String phoneNumber;
+}
