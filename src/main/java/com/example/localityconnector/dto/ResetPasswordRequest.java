4d4267cbaffa9e6cb5302cf865c1f65d09b49f15
Ritik Swaraj <swarajritik@gmail.com>
2026-07-08 22:02:48 +0530

    Add data transfer object (ResetPasswordRequest.java)

---
+++ src/main/java/com/example/localityconnector/dto/ResetPasswordRequest.java
@@ -0,0 +1,16 @@
+package com.example.localityconnector.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ResetPasswordRequest {
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "New password must be at least 8 characters long")
+    private String newPassword;
+}
